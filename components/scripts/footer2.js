73cf4b2c5cd3e84e1d15b32536240840613e92b3
bashar alam <bashar.alam@carrum.co.in>
2025-04-13 11:37:06 +0530

    added some data

--- components/scripts/footer2.js
+++ components/scripts/footer2.js
@@ -65,7 +65,7 @@ const setFooter = () => {
               <path
                 d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
             </svg>
-            123 Luxury Lane, Design District
+            Work & Office Address - 56/B, Damodar Park Industrial Area, Delhi - 110095
           </div>
           <div class="contact-info">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
@@ -79,14 +79,14 @@ const setFooter = () => {
               <path
                 d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z" />
             </svg>
-            +1 (555) 123-4567
+             +91 7982166639
           </div>
           <div class="contact-info">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
               <path
                 d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 14h-4v4h-4v-4h-4v-4h4v-4h4v4h4v4z" />
             </svg>
-            Mon - Sat: 9AM - 7PM
+            Wednesday - Monday: 9AM - 7PM
           </div>
         </div>
       </div>
